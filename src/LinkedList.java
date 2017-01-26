198e23b1eb37df1e39ebbd1692b01415f2afb59b
navspeak <navneet.sahay@gmail.com>
2017-01-26 19:01:21 +0530

    Tests added

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -47,11 +47,15 @@ public class LinkedList<T> implements Iterable<T>{
 
 	// getFirst
 	public T getFirst(){
+		if (isEmpty())
+			throw new NoSuchElementException("Empty List");
 		return head.data;
 	}
 
 	// getLast
 	public T getLast(){
+		if (isEmpty())
+			throw new NoSuchElementException("Empty List");
 		Node<T> ptr = head;
 		while (ptr!=null && ptr.next != null){
 			ptr = ptr.next;
@@ -59,8 +63,21 @@ public class LinkedList<T> implements Iterable<T>{
 		return ptr.data;
 	}
 
+	// get (index) - 0 based
+	public T get(int index){
+		if (isEmpty())
+			throw new NoSuchElementException("Empty List");
+		Node<T> ptr = head;
+		for (int i = 0; i < index; i++){
+			ptr = ptr.next;
+		}
+		return ptr.data;
+			
+	}
 	// removeFirst
 	public T removeFirst(){
+		if (isEmpty())
+			throw new NoSuchElementException("Empty List");
 		T data = head.data;
 		head = head.next;
 		return data;
@@ -68,6 +85,8 @@ public class LinkedList<T> implements Iterable<T>{
 
 	// removeLast
 	public T removeLast(){
+		if (isEmpty())
+			throw new NoSuchElementException("Empty List");
 		Node<T> ptr = head;
 		while (ptr!=null && ptr.next != null){
 			ptr = ptr.next;
@@ -75,6 +94,18 @@ public class LinkedList<T> implements Iterable<T>{
 		return ptr.data;
 	}
 
+	// remove (key)
+	public T remove(T key){
+		for (Node<T> ptr = head; ptr !=null; ptr = ptr.next){
+			if (ptr.next.data == key){
+				T ret = ptr.next.data;
+				ptr.next = ptr.next.next;
+				return ret;
+			}
+		}
+		throw new NoSuchElementException();
+	}
+	
 	//reverse
 	public LinkedList<T> reverse(){
 		LinkedList<T> twin = new LinkedList<T>();
@@ -83,6 +114,29 @@ public class LinkedList<T> implements Iterable<T>{
 		return twin;
 	}
 
+	// insertBefore
+	public void insertBefore(T key, T data){
+		for (Node<T> ptr = head; ptr!=null ; ptr = ptr.next){
+			if (ptr.next.data == key) {
+				Node<T> tmp = new Node<>(data, ptr.next);
+				ptr.next = tmp;
+				return;
+			}
+		}
+
+	}
+	
+	// insertAfter
+	public void insertAfter(T key, T data){
+		for (Node<T> ptr = head; ptr!=null ; ptr = ptr.next){
+			if (ptr.data == key) {
+				Node<T> tmp = new Node<>(data, ptr.next);
+				ptr.next = tmp;
+				return;
+			}
+		}
+
+	}
 	//copy
 	public LinkedList<T> copy(){
 		LinkedList<T> twin = new LinkedList<T>();
@@ -91,20 +145,90 @@ public class LinkedList<T> implements Iterable<T>{
 		//twin.addLast(data); --> O(n2)
 		return twin.reverse();
 	}
-
+	//copy2
+	public LinkedList<T> copy2(){ // More efficient
+		LinkedList<T> twin = new LinkedList<T>();
+		twin.head = new Node<>(head.data, null);
+		Node<T> ptrOrg = this.head;
+		Node<T> ptrTwin = twin.head;
+		while (ptrOrg.next != null){
+			ptrOrg = ptrOrg.next;
+			Node<T> tmp = new Node<>(ptrOrg.data, null);
+			ptrTwin.next = tmp;
+			ptrTwin = ptrTwin.next;
+		}
+		return twin;
+	}
+	
+	//copy
+	public LinkedList<T> copy3(){
+		LinkedList<T> twin = new LinkedList<T>();
+		for (Node<T> ptr = head; ptr!=null ; ptr = ptr.next)
+			//twin.addFirst(ptr.data); // O(n)
+		twin.addLast(ptr.data); //--> O(n2)
+		return twin;
+	}
+	
+	// Introduce loop in the LinkedList
+	public void introduceLoop(int loopIndex){
+		Node<T> ptr1 = head;
+		Node<T> ptr2 = head;
+		for(int i = 0; i < loopIndex; i++) {
+			ptr1 = ptr1.next;
+		}
+		
+		for(ptr2 = head; ptr2.next !=null ; ptr2= ptr2.next);
+		
+		ptr2.next = ptr1;
+	}
+	
+	// test loop
+	public boolean testLoop(){
+		if (isEmpty())
+			return false;
+		Node<T> tortoise = head;
+		Node<T> hare = head;
+		
+		while (hare != null && hare.next != null){
+			tortoise = tortoise.next;
+			hare  =  hare.next.next;
+			if (hare == tortoise)
+				return true;
+		}
+		return false;
+	}
+	
+	// find mid (second of two in case of even)
+	public T findMid(){
+		if (isEmpty())
+			throw new RuntimeException("Empty List");
+		Node<T> tortoise = head;
+		Node<T> hare = head;
+		
+		while (hare != null && hare.next != null){
+			tortoise = tortoise.next;
+			hare  =  hare.next.next;
+			if (hare == tortoise)
+				throw new RuntimeException("Loop Detected");
+		}
+		return tortoise.data;
+	}
 
 	private static class Node<T> {
 		T data;
-		Node next;
+		Node<T> next;
 
 		public Node(T data, Node<T> next) {
 			this.data = data;
 			this.next = next;
 		}
+
 	}
 
 	@Override
 	public String toString() {
+		if (this.testLoop())
+			throw new RuntimeException("Loop detected. toString will enter infinite loop");
 		StringBuilder sb = new StringBuilder();
 		for(T data : this){
 			sb.append(data).append("->");
@@ -180,12 +304,39 @@ public class LinkedList<T> implements Iterable<T>{
 		System.out.println(list1.copy());
 		System.out.println(list2.copy());
 		
+		//copy the list
+		System.out.println("Copy 2");
+		System.out.println(list1.copy2());
+		System.out.println(list2.copy2());
+		
 		//create a linked list
 		Integer[] array = (Integer[]) Arrays.asList(1,2,3,4,5).toArray();
 		LinkedList<Integer> list3 = new LinkedList<Integer>(array);
 		System.out.println(list3);
 		
-
+		// insert before
+		// 1->2->_->4->5->null
+		LinkedList<String> list4 = new LinkedList<String>();
+		list4.addLast("1");
+		list4.addLast("2");
+		list4.addLast("4");
+		list4.addLast("5");
+		list4.insertBefore("4", "3");
+		System.out.println(list2);
+		
+		// insert After
+		// 1->2->3->_->5->null
+		LinkedList<String> list5 = new LinkedList<String>();
+		list5.addLast("1");
+		list5.addLast("2");
+		list5.addLast("3");
+		list5.addLast("5");
+		list5.insertAfter("3", "4");
+		System.out.println(list5);
+		
+		//get
+		System.out.println(list1.get(0));
+		System.out.println(list1.get(1));
 
 	}
 
---
+++ src/LinkedListTest.java
@@ -0,0 +1,198 @@
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.NoSuchElementException;
+import java.util.Random;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Categories.ExcludeCategory;
+import org.junit.experimental.theories.ParametersSuppliedBy;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+
+public class LinkedListTest {
+	
+	private static LinkedList<Integer> emptyList;
+	private static LinkedList<Integer> ListWithOneElement;
+	private static LinkedList<Integer> ListWithtwoElement;
+	private static LinkedList<Integer> ListWiththreeElement;
+	private static LinkedList<Integer> ListWithtenElement;
+	private static LinkedList<Integer> ListWithTenThousandElement;
+	
+	@BeforeClass
+	public static void init(){
+		emptyList = new LinkedList<Integer>();
+		Integer[] oneElementArray = new Integer[] {1};
+		Integer[] twoElementArray = new Integer[] {1,2};
+		Integer[] threeElementArray = new Integer[] {1,2,3};
+		Integer[] tenElementArray = new Integer[] {1,2,3,4,5,6,7,8,9};
+		Integer[] thousandElementArray = new Integer[10000000];
+		for (int i = 0; i < thousandElementArray.length; i++){
+			thousandElementArray[i] = i;
+		}
+		ListWithOneElement = new LinkedList<Integer>(oneElementArray);
+		ListWithtwoElement = new LinkedList<Integer>(twoElementArray);
+		ListWiththreeElement = new LinkedList<Integer>(threeElementArray);
+		ListWithtenElement = new LinkedList<Integer>(tenElementArray);
+		ListWithTenThousandElement = new LinkedList<Integer>(thousandElementArray);
+		
+	}
+	
+	
+	@Test
+	public final void testIsEmpty() {
+		assertTrue(emptyList.isEmpty());
+		assertFalse(ListWithtenElement.isEmpty());
+	}
+	
+	@Test
+	public final void testAddFirst() {
+		
+		LinkedList<Integer> twinEmptyList = emptyList.copy();
+		twinEmptyList.addFirst(9);
+		assertEquals("9->NULL", twinEmptyList.toString());
+		
+		LinkedList<Integer> twinThreeList = ListWiththreeElement.copy();
+		twinThreeList.addFirst(9);
+		assertEquals("9->3->2->1->NULL", twinThreeList.toString());
+
+	}
+	
+	@Test
+	public final void testAddLast() {
+		LinkedList<Integer> twinEmptyList = emptyList.copy();
+		twinEmptyList.addLast(9);
+		assertEquals("9->NULL", twinEmptyList.toString());
+		
+		LinkedList<Integer> twinThreeList = ListWiththreeElement.copy();
+		twinThreeList.addLast(9);
+		assertEquals("3->2->1->9->NULL", twinThreeList.toString());
+		
+	}
+	
+	@Test
+	public final void testReverse() {
+		LinkedList<Integer> twinEmptyList = emptyList.copy();
+		twinEmptyList.reverse();
+		assertEquals("NULL", twinEmptyList.toString());
+	
+		LinkedList<Integer> twinThreeList = ListWiththreeElement.copy();
+		assertEquals("1->2->3->NULL", twinThreeList.reverse().toString());
+		
+	}
+	
+	@Test(expected=NoSuchElementException.class)
+	public final void testRemoveFirstEmpty() {
+		LinkedList<Integer> twinEmptyList = emptyList.copy();
+		twinEmptyList.removeFirst();
+	}
+	
+	@Test(expected=NoSuchElementException.class)
+	public final void testRemoveLastEmpty() {
+		LinkedList<Integer> twinEmptyList = emptyList.copy();
+		twinEmptyList.removeLast();
+	}
+	
+	@Test
+	public final void testCopy() {
+		Long start = System.currentTimeMillis();
+		LinkedList<Integer> twinList = ListWithTenThousandElement.copy();
+		Long end = System.currentTimeMillis();
+		System.out.println(end - start);
+	}
+	
+	@Test
+	public final void testCopy2() {
+		Long start = System.currentTimeMillis();
+		LinkedList<Integer> twinList = ListWithTenThousandElement.copy2();
+		Long end = System.currentTimeMillis();
+		System.out.println(end - start);
+	}
+	
+	@Test
+	public final void testGet() {
+		assertEquals(5, ListWithtenElement.reverse().get(4).intValue());
+	}
+	
+	@Test
+	public final void testLoop() {
+		LinkedList<Integer> twinThreeList = ListWiththreeElement.copy();
+		twinThreeList.introduceLoop(1);
+		assertTrue(twinThreeList.testLoop());
+	}
+	
+	@Test
+	public final void testLoop1() {
+		LinkedList<Integer> twinThreeList = ListWiththreeElement.copy();
+		twinThreeList.toString();
+		assertFalse(twinThreeList.testLoop());
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public final void testLoop3() {
+		LinkedList<Integer> twinThreeList = ListWiththreeElement.copy();
+		twinThreeList.introduceLoop(1);
+		twinThreeList.toString();
+	}
+	
+	@Test
+	public final void testLoop4() {
+		LinkedList<Integer> twinEmptyList = emptyList.copy();
+		assertFalse(twinEmptyList.testLoop());
+	}
+	
+	@Test
+	public final void testMid() {
+		LinkedList<Integer> twinThreeList = ListWiththreeElement.copy();
+		assertEquals(2,twinThreeList.findMid().intValue());
+	}
+	
+	@Test
+	public final void testMid1() {
+		LinkedList<Integer> twinThreeList = ListWiththreeElement.copy();
+		twinThreeList.addFirst(9);
+		assertEquals(2,twinThreeList.findMid().intValue());
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public final void testMid3() {
+		LinkedList<Integer> twinEmptyList = emptyList.copy();
+		twinEmptyList.findMid();
+	}
+	
+/*	@Test
+	public final void testCopy3() {
+		Long start = System.currentTimeMillis();
+		LinkedList<Integer> twinList = ListWithTenThousandElement.copy3();
+		Long end = System.currentTimeMillis();
+		System.out.println(end - start);
+	}*/
+	
+/*	@Test
+	public final void testRemoveLast() {
+		assertTrue(emptyList.isEmpty());
+		assertFalse(ListWithtenElement.isEmpty());
+	}
+	
+	@Test
+	public final void testGet() {
+		assertTrue(emptyList.isEmpty());
+		assertFalse(ListWithtenElement.isEmpty());
+	}
+	
+	@Test
+	public final void testCopy() {
+		assertTrue(emptyList.isEmpty());
+		assertFalse(ListWithtenElement.isEmpty());
+	}
+	
+	@Test
+	public final void testCopy2() {
+		assertTrue(emptyList.isEmpty());
+		assertFalse(ListWithtenElement.isEmpty());
+	}*/
+
+}
