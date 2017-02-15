05183a98e31c9fcde931c152e307a3c9e179b053
navspeak <navneet.sahay@gmail.com>
2017-02-15 23:15:18 +0530

    HeapSort fixes

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -6,110 +6,114 @@ import java.util.Arrays;
  * REMOVAL OF THE MAXIMUM ELEMENT IS DONE N TIMES,FOLLOWED BY HEAPIFY
  * INSERTION AND REMOVAL HAVE LOG N TIME COMPLEXITY SO DOING IT FOR N ELEMENTS MEANS:
  * O(NLOGN)
- * NOT STABLE, NOT ADAPTIVE
  * @author knavneet
  *
  */
 
 /**
  * Sample run:
-[12, 35, 87, 26, 9, 28, 7]
-calling heapify on : [12, 35, 87, 26, 9, 28, 7] index = 2 end = 6
-After Heapify : [87, 35, 28, 26, 9, 12, 7]
-calling heapify on : [7, 35, 28, 26, 9, 12, 87] index = 2 end = 5
-After Heapify : [35, 26, 28, 7, 9, 12, 87]
-calling heapify on : [12, 26, 28, 7, 9, 35, 87] index = 1 end = 4
-After Heapify : [28, 26, 12, 7, 9, 35, 87]
-calling heapify on : [9, 26, 12, 7, 28, 35, 87] index = 1 end = 3
-After Heapify : [26, 9, 12, 7, 28, 35, 87]
-calling heapify on : [7, 9, 12, 26, 28, 35, 87] index = 0 end = 2
-After Heapify : [12, 9, 7, 26, 28, 35, 87]
-calling heapify on : [7, 9, 12, 26, 28, 35, 87] index = 0 end = 1
-After Heapify : [9, 7, 12, 26, 28, 35, 87]
-calling heapify on : [7, 9, 12, 26, 28, 35, 87] index = 0 end = 0
-After Heapify : [7, 9, 12, 26, 28, 35, 87]
-[7, 9, 12, 26, 28, 35, 87]
- *
+[12, 35, 87, 111, 26, 9, 28, 7, 100, 67, 9, 204]
+calling heapify on : [12, 35, 87, 111, 26, 9, 28, 7, 100, 67, 9, 204] end = 11
+After Heapify      : [204, 111, 12, 35, 67, 87, 28, 7, 100, 26, 9, 9]
+calling heapify on : [9, 111, 12, 35, 67, 87, 28, 7, 100, 26, 9, 204] end = 10
+After Heapify      : [111, 9, 87, 100, 67, 12, 28, 7, 35, 26, 9, 204]
+calling heapify on : [9, 9, 87, 100, 67, 12, 28, 7, 35, 26, 111, 204] end = 9
+After Heapify      : [100, 9, 87, 9, 67, 12, 28, 7, 35, 26, 111, 204]
+calling heapify on : [26, 9, 87, 9, 67, 12, 28, 7, 35, 100, 111, 204] end = 8
+After Heapify      : [87, 67, 26, 35, 9, 12, 28, 7, 9, 100, 111, 204]
+calling heapify on : [9, 67, 26, 35, 9, 12, 28, 7, 87, 100, 111, 204] end = 7
+After Heapify      : [67, 9, 28, 35, 9, 12, 26, 7, 87, 100, 111, 204]
+calling heapify on : [7, 9, 28, 35, 9, 12, 26, 67, 87, 100, 111, 204] end = 6
+After Heapify      : [35, 7, 28, 9, 9, 12, 26, 67, 87, 100, 111, 204]
+calling heapify on : [26, 7, 28, 9, 9, 12, 35, 67, 87, 100, 111, 204] end = 5
+After Heapify      : [28, 9, 26, 9, 7, 12, 35, 67, 87, 100, 111, 204]
+calling heapify on : [12, 9, 26, 9, 7, 28, 35, 67, 87, 100, 111, 204] end = 4
+After Heapify      : [26, 9, 12, 9, 7, 28, 35, 67, 87, 100, 111, 204]
+calling heapify on : [7, 9, 12, 9, 26, 28, 35, 67, 87, 100, 111, 204] end = 3
+After Heapify      : [12, 9, 7, 9, 26, 28, 35, 67, 87, 100, 111, 204]
+calling heapify on : [9, 9, 7, 12, 26, 28, 35, 67, 87, 100, 111, 204] end = 2
+After Heapify      : [9, 9, 7, 12, 26, 28, 35, 67, 87, 100, 111, 204]
+calling heapify on : [7, 9, 9, 12, 26, 28, 35, 67, 87, 100, 111, 204] end = 1
+After Heapify      : [9, 7, 9, 12, 26, 28, 35, 67, 87, 100, 111, 204]
+calling heapify on : [7, 9, 9, 12, 26, 28, 35, 67, 87, 100, 111, 204] end = 0
+After Heapify      : [7, 9, 9, 12, 26, 28, 35, 67, 87, 100, 111, 204]
+[7, 9, 9, 12, 26, 28, 35, 67, 87, 100, 111, 204]
  */
 
 public class HeapSort {
 
+	static int indices[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 	public static void main(String[] args) {
-		int arr [] = {12, 35, 87, 26, 9, 28, 7};
+		int arr [] = {12, 35, 87, 111, 26, 9, 28, 7, 100, 67, 9, 204};
 		System.out.println(Arrays.toString(arr));
-		//heapify(arr, getParentIndex(arr.length - 1), arr.length - 1);
 		heapsort(arr);
 		System.out.println(Arrays.toString(arr));
-		//87, 35, 28, 26, 9, 12, 7
-		//7, 35, 28, 26, 9, 12, 87
 	}
 
-    // Step 1: Heapify an Array
+	// Step 1: Heapify an Array
 	// Step 2. Remove Max Element, reduce array boundry and Heapify again (siftdown actually)
 	public static void heapsort(int [] arr){
-		int parentIndex = getParentIndex(arr.length - 1);
 		int endOfArray = arr.length - 1;
 		while(endOfArray >= 0){
-			System.out.println("calling heapify on : " + Arrays.toString(arr) + " index = " + parentIndex + " end = " + endOfArray);
-			heapify(arr, parentIndex, endOfArray);
-			System.out.println("After Heapify : " + Arrays.toString(arr));
+			System.out.println("calling heapify on : " + Arrays.toString(arr) + " end = " + endOfArray);
+			heapify(arr,  endOfArray);
+			System.out.println("After Heapify      : " + Arrays.toString(arr));
 			swap(arr, 0, endOfArray);
 			endOfArray--;
-			parentIndex = getParentIndex(endOfArray);
 		}
 
 
 	}
-
-	private static void heapify(int[] arr, int index, int endofarray)
+    
+	// input array and end of array
+	private static void heapify(int[] arr,  int endofarray)
 	{
+		int index = getParentIndex(endofarray); // last parent index
 		if (!hasLeftChild(index, endofarray))
 			return;
 
-		int parentIndex = index;
-		while (hasLeftChild(parentIndex, endofarray)){
-			int greaterIndex = getLeftChildIndex(parentIndex);
-			if (hasRightChild(parentIndex, endofarray)){
-				greaterIndex = (arr[getLeftChildIndex(parentIndex)] > arr[getRightChildIndex(parentIndex)])? getLeftChildIndex(parentIndex) : getRightChildIndex(parentIndex);
+		while (hasLeftChild(index, endofarray)){
+			int greaterIndex = getLeftChildIndex(index);
+			if (hasRightChild(index, endofarray)){
+				greaterIndex = (arr[getLeftChildIndex(index)] > arr[getRightChildIndex(index)])? getLeftChildIndex(index) : getRightChildIndex(index);
 			}
-			if (arr[parentIndex] < arr[greaterIndex]){
-				swap(arr, parentIndex, greaterIndex);
-				parentIndex =  greaterIndex;
-			} else {
-				parentIndex--;
-			}
+			if (arr[index] < arr[greaterIndex]){
+				swap(arr, index, greaterIndex);
+			} 
+			index--;
 		}
 	}
 
-	private static void swap(int[] arr, int i, int j) {
-		int tmp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = tmp;
-	}
 
-	private static int getRightChildIndex(int i) {
-		return (2*i + 2);
-	}
+private static void swap(int[] arr, int i, int j) {
+	int tmp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = tmp;
+}
 
-	private static int getLeftChildIndex(int i) {
-		return (2*i + 1);
-	}
+private static int getRightChildIndex(int i) {
+	return (2*i + 2);
+}
 
-	private static int getParentIndex(int i) {
-		return (i - 1)/2;
-	}
+private static int getLeftChildIndex(int i) {
+	return (2*i + 1);
+}
 
-	private static boolean hasLeftChild(int index, int endofarray) {
-		return (index >= 0 && getLeftChildIndex(index)<=endofarray);
-	}
+private static int getParentIndex(int i) {
+	return (i - 1)/2;
+}
 
-	private static boolean hasRightChild(int index, int endofarray) {
-		return (index >= 0 && getRightChildIndex(index)<=endofarray);
-	}
+private static boolean hasLeftChild(int index, int endofarray) {
+	return (index >= 0 && getLeftChildIndex(index)<=endofarray);
+}
 
-	private boolean hasParent(int index) {
-		return (getParentIndex(index)>=0);
-	}
+private static boolean hasRightChild(int index, int endofarray) {
+	return (index >= 0 && getRightChildIndex(index)<=endofarray);
+}
+
+private boolean hasParent(int index) {
+	return (getParentIndex(index)>=0);
+}
 
 }
