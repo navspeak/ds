f43c2f5c571a9b83c448eea4e36683352225788b
navspeak <navneet.sahay@gmail.com>
2017-02-17 08:23:45 +0530

    comments

--- src/MaxSubArraySum.java
+++ src/MaxSubArraySum.java
@@ -3,18 +3,22 @@
 public class MaxSubArraySum {
 	public static void main(String[] args) {
 		int[] arr = {2, -9, 5, 1, -4, 6, 0, -7, 8};
+		// For all positive nos. we don't need kadane's. We 
+		// can simply add all nos.
 		System.out.println(findMaxSubArraySum(arr));
 	}
 
 	private static int findMaxSubArraySum(int[] arr) {
 		int size = arr.length;
-		int max_so_far = arr[0];
-		int curr_max = arr[0];
+		int global_max = arr[0];//global max
+		int local_max = arr[0]; 
 		for (int i = 1; i < size ; i++){
-			curr_max = Math.max(arr[i], curr_max + arr[i]);
-			max_so_far = Math.max(max_so_far, curr_max);
+			// local_max is current element or current elem + curr max till now
+			local_max = Math.max(arr[i], local_max + arr[i]);
+			//if curr_max > global_max, update global max
+			global_max = Math.max(global_max, local_max);
 		}
-		return max_so_far;
+		return local_max;
 	}
 
 }
