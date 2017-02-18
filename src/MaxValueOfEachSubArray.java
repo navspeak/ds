09e0e686da2ee0bf5d9e7ecc4fae918b8cc8f4a0
navspeak <navneet.sahay@gmail.com>
2017-02-18 09:02:56 +0530

    maxofsubarray

--- src/MaxValueOfEachSubArray.java
+++ src/MaxValueOfEachSubArray.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Deque;
 import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 
 // The time complexity of this algorithm is O(n) 
@@ -26,11 +29,23 @@ public class MaxValueOfEachSubArray {
 
 	public static void main(String[] args) {
 		int arr[] = {9,6,11,8,10,5,14,13,93,14};
+	/*	9,6,11,8      = 11
+		6,11,8,10     = 11
+		11,8,10,5     = 11
+		8,10,5,14     = 14
+		10,5,14,13    = 14
+		5,14,13,93    = 93
+		14,13,93,14   = 93 */
+		
 		List<Integer> output = MaxValueOfEachSubArray(arr, 4);
 		System.out.println(Arrays.toString(output.toArray()));
+		
+		List<Integer> output2 = MaxValueOfEachSubArray2(arr, 4);
+		System.out.println(Arrays.toString(output2.toArray()));
 	}
 
-	private static List<Integer> MaxValueOfEachSubArray(int[] arr, int size) {
+	// O(N) time and O(K) space - use Dequeue
+	public static List<Integer> MaxValueOfEachSubArray(int[] arr, int size) {
 		Deque<Integer> list = new ArrayDeque<Integer>();
 		List<Integer> output = new ArrayList<Integer>();
 		// Step 1: Adding indices to the list for first 'k' elements
@@ -58,6 +73,25 @@ public class MaxValueOfEachSubArray {
 				list.removeLast();
 			list.addLast(i);
 		}
+		output.add(arr[list.getFirst()]);
+		return output;
+	}
+
+	// O(Nlogk) time and O(K) space - use AVL (in fact Red Black tree as TreeSet uses Red Black Tree)
+	public static List<Integer> MaxValueOfEachSubArray2(int[] arr, int size) {
+		SortedSet<Integer> set = new TreeSet<Integer>();
+		List<Integer> output = new ArrayList<Integer>();
+		for (int i = 0; i < size; i++) {
+			set.add(arr[i]);
+		}
+		
+		for (int i = size; i < arr.length; i++) {
+			output.add(set.last());
+			set.remove(arr[i-size]);
+			set.add(arr[i]);
+		}	
+		
+		output.add(set.last());
 		return output;
 	}
 
