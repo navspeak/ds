e8c06338c3511e21a1fd6f34218d839c523f6063
navspeak <navneet.sahay@gmail.com>
2017-04-15 09:55:24 +0530

    Skyline Problem

--- src/StreamingMedian.java
+++ src/StreamingMedian.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
@@ -6,19 +11,20 @@ import java.util.PriorityQueue;
 public class StreamingMedian {
 
 	public static void main(String[] args){
-		int[] arr = { 1,8,7,5,2,6,5, 7, 4, 10};
+		int[] arr = { 1,1,8,7,5,2,6,5, 7, 4, 10};
 		System.out.println(Arrays.toString(arr));
 		System.out.println(Arrays.toString(getStreamingMedian(arr)));
-
+		
 	}
 
 	public static double[] getStreamingMedian(int[] arr){
-		PriorityQueue<Integer> lowers = new PriorityQueue<Integer>(new Comparator<Integer>() {
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				return -1 * o1.compareTo(o2);
-			}
-		}); // MaxHeap;
+//		PriorityQueue<Integer> lowers = new PriorityQueue<Integer>(new Comparator<Integer>() {
+//			@Override
+//			public int compare(Integer o1, Integer o2) {
+//				return -1 * o1.compareTo(o2);
+//			}
+//		}); // MaxHeap;
+		PriorityQueue<Integer> lowers = new PriorityQueue<>(Collections.reverseOrder());
 		PriorityQueue<Integer> highers = new PriorityQueue<Integer>(); // MinHeap;
 		double[] medians = new double[arr.length];
 		for (int i = 0; i < arr.length; i++){
@@ -40,6 +46,13 @@ public class StreamingMedian {
 
 	private static double getMedian(PriorityQueue<Integer> lowers,
 			PriorityQueue<Integer> highers) {
+		System.out.print("Lower Heap : "); 
+		lowers.stream().forEach(num -> System.out.print(num +", "));
+		System.out.println();
+		System.out.print("Higher Heap : "); 
+		highers.stream().forEach(num -> System.out.print(num +", "));
+		System.out.println();
+		System.out.println("======================");
 		if (lowers.size() == highers.size()){
 			double ret = ((double)lowers.peek() + highers.peek())/2;
 			return ret;
@@ -61,3 +74,40 @@ public class StreamingMedian {
 	}
 
 }
+
+//
+//[1, 1, 8, 7, 5, 2, 6, 5, 7, 4, 10]
+//Lower Heap : 1, 
+//Higher Heap : 
+//======================
+//Lower Heap : 1, 
+//Higher Heap : 1, 
+//======================
+//Lower Heap : 1, 
+//Higher Heap : 1, 8, 
+//======================
+//Lower Heap : 1, 1, 
+//Higher Heap : 7, 8, 
+//======================
+//Lower Heap : 1, 1, 
+//Higher Heap : 5, 8, 7, 
+//======================
+//Lower Heap : 2, 1, 1, 
+//Higher Heap : 5, 8, 7, 
+//======================
+//Lower Heap : 2, 1, 1, 
+//Higher Heap : 5, 6, 7, 8, 
+//======================
+//Lower Heap : 5, 2, 1, 1, 
+//Higher Heap : 5, 6, 7, 8, 
+//======================
+//Lower Heap : 5, 2, 1, 1, 
+//Higher Heap : 5, 6, 7, 8, 7, 
+//======================
+//Lower Heap : 5, 4, 1, 1, 2, 
+//Higher Heap : 5, 6, 7, 8, 7, 
+//======================
+//Lower Heap : 5, 4, 1, 1, 2, 
+//Higher Heap : 5, 6, 7, 8, 7, 10, 
+//======================
+//[1.0, 1.0, 1.0, 4.0, 5.0, 3.5, 5.0, 5.0, 5.0, 5.0, 5.0]
