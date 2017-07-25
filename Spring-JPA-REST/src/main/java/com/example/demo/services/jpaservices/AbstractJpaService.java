18494683c94241aeca27f6fe67f4a71cbc3d8138
navspeak <navneet.sahay@gmail.com>
2017-07-25 22:36:56 +0530

    reorg

--- Spring-JPA-REST/src/main/java/com/example/demo/services/jpaservices/AbstractJpaService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.services.jpaservices;
-
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
-
-public abstract class AbstractJpaService {
-	@PersistenceUnit
-	public EntityManagerFactory emf;
-}
